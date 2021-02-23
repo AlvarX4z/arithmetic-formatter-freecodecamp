8f549e823f5d57f8c3379401f74af22e20cc8dee
Alvaro de Francisco <alfrasan@gmail.com>
2021-02-23 17:50:55 +0100

    Removed Unnecesary Import
    Minor fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # This entrypoint file to be used in development. Start by reading README.md
 from arithmetic_arranger import arithmetic_arranger
-from unittest import main
 
 # Testing operations are from the challenge
 print(arithmetic_arranger(["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]))
